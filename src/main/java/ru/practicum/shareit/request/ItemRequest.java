86ddee47c7b184a208af0afe29e4f656af728fdb
Jaroslav <120812496+Yarik-Yarik@users.noreply.github.com>
2024-09-11 12:14:29 +0500

    fix:Postman tests

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.shareit.user.model.User;
@@ -18,6 +19,7 @@ import java.time.LocalDate;
  * TODO Sprint add-item-requests.
  */
 @Entity
+@Table(name = "requests")
 @Data
 @AllArgsConstructor
 
