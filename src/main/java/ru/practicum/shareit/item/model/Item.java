fc3e4db43e9879978996c0771da0b8ab053bb2db
Jaroslav <120812496+Yarik-Yarik@users.noreply.github.com>
2024-09-11 12:35:28 +0500

    fix:Postman tests

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -49,9 +49,9 @@ public class Item {
     @Transient
     private Booking nextBooking;
 
-    @ManyToOne
-    @JoinColumn(name = "request_id")
-    private ItemRequest request;
+//    @ManyToOne
+//    @JoinColumn(name = "request_id")
+//    private ItemRequest request;
 
 /*    public Item(Long id, String name, String description, Boolean available) {
         this.id = id;
