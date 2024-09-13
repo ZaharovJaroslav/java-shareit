0f1f9e2baa52755aca44822fa5e65f495a9483c3
Jaroslav <120812496+Yarik-Yarik@users.noreply.github.com>
2024-09-13 22:31:07 +0500

    CheckStyle

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking.service;
 
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.NewBookingRequestDto;
-import ru.practicum.shareit.booking.model.BookingStatus;
 
 import java.util.Collection;
 
